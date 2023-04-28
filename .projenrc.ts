ecd2d878720224ceb49bdc19679defcc957c2ab1
Derek Kershner <dkershner@dkershner.com>
2023-04-28 14:32:39 -0700

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Derek Kershner',
+  authorAddress: 'dkershner@dkershner.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'cdk-nextjs-standalone-ecs',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/dkershner/cdk-nextjs-standalone-ecs.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
